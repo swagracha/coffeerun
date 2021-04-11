78915586e90dd610bcf12ccc1da9dff37bf557be
swagracha <67877748+swagracha@users.noreply.github.com>
2021-04-10 21:53:36 -0700

    Add files via upload

--- coffeerun/scripts/paymentForm.js
+++ coffeerun/scripts/paymentForm.js
@@ -3,7 +3,6 @@
     var FORM_SELECTOR = '[data-payment="form"]';
     var App = window.App;
     // eslint-disable-next-line no-unused-vars
-    var DataStore = App.DataStore;
     var FormHandler = App.FormHandler;
     var formHandler = new FormHandler(FORM_SELECTOR);
 
---
+++ coffeerun/scripts/validation.js
@@ -0,0 +1,15 @@
+(function (window) {
+    'use strict';
+    var App = window.App || {};
+
+    var Validation = {
+        isCompanyEmail: function (email) {
+            return /.+@swagracha\.com$/.test(email);
+        }
+    }
+
+   
+    App.Validation = Validation;
+    window.App = App;
+})(window);
+
